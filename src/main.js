cb3391991d031a85b607798a916a4c3b59515858
Pankaj Mondal <pankajgurudeb@gmail.com>
2016-08-26 23:46:29 +0530

    Modifications to main.js

---
+++ src/main.js
@@ -0,0 +1,17 @@
+const electron = require("electron");
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+app.on("ready", _ => {
+    mainWindow = new BrowserWindow({
+        height: 400,
+        width: 400
+    });
+
+    mainWindow.on("closed", _ => {
+        mainWindow = null;
+    });
+});
